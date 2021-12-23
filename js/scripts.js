5640fc8c5463bfda69ec327f5035bb2c92e56834
Brian Alves Moreira <hey@brianmoreira.com>
2021-12-23 01:32:59 -0300

    Cria script para fazer card ativo

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,8 @@ $(document).foundation();
 $('.title-bar').click( function() {
 	$('.header').toggleClass('active');
 });
+
+$('.sec--solutions .card').click( function() {
+	$(this).toggleClass('active');
+	$(this).siblings().removeClass('active');
+});
