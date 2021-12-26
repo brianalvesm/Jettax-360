819167c617a64092cbad97f7b7044517ced0e4db
Brian Alves Moreira <hey@brianmoreira.com>
2021-12-26 16:38:46 -0300

    Melhora componente de section Como Funciona

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,45 @@
 $(document).foundation();
 
-$('.title-bar').click( function() {
-	$('.header').toggleClass('active');
-});
+$(document).ready(function(){
+	(function ($) {
 
-$('.sec--solutions .card').click( function() {
-	$(this).addClass('active');
-	$(this).siblings().removeClass('active');
+		$('.title-bar').click( function() {
+			$('.header').toggleClass('active');
+		});
+
+		$('.sec--solutions .card').click( function() {
+			$(this).addClass('active');
+			$(this).siblings().removeClass('active');
+		});
+
+		var owlMobile = function() {
+			if ($(window).width() < 1024) {
+				$('.owl-carousel').owlCarousel({
+				  center: false,
+				  loop:false,
+				  margin:10,
+				  nav:false,
+				  dots:false,
+				  autoplay:false,
+				  responsive : {
+				    // breakpoint from 0 up
+				    0 : {
+				    	items: 2
+				    },
+				    // breakpoint from 640 up
+				    640 : {
+				    	items: 3
+				    }
+				  }
+				});
+			};
+		};
+
+		owlMobile();
+
+		$(window).resize(function() {
+		  owlMobile();
+		});
+
+	})(jQuery);
 });
