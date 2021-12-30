b1e044a139dbd46e81bef7c068a0a85efb7992b0
Brian Alves Moreira <hey@brianmoreira.com>
2021-12-30 18:08:01 -0300

    Ajustes e melhorias gerais

--- js/scripts.js
+++ js/scripts.js
@@ -12,35 +12,25 @@ $(document).ready(function(){
 			$(this).siblings().removeClass('active');
 		});
 
-		var owlMobile = function() {
-			if ($(window).width() < 1024) {
-				$('.owl-carousel').owlCarousel({
-				  center: false,
-				  loop:false,
-				  margin:10,
-				  nav:false,
-				  dots:false,
-				  autoplay:false,
-				  URLhashListener:true,
-				  startPosition: 'URLHash',
-				  responsive : {
-				    // breakpoint from 0 up
-				    0 : {
-				    	items: 2
-				    },
-				    // breakpoint from 640 up
-				    640 : {
-				    	items: 3
-				    }
-				  }
-				});
-			};
-		};
-
-		owlMobile();
-
-		$(window).resize(function() {
-		  owlMobile();
+		$('.owl-carousel').owlCarousel({
+		  center: false,
+		  loop:false,
+		  margin:10,
+		  nav:false,
+		  dots:false,
+		  autoplay:false,
+		  URLhashListener:true,
+		  startPosition: 'URLHash',
+		  responsive : {
+		    // breakpoint from 0 up
+		    0 : {
+		    	items: 2
+		    },
+		    // breakpoint from 640 up
+		    640 : {
+		    	items: 3
+		    }
+		  }
 		});
 
 	})(jQuery);
