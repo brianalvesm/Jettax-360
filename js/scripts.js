fd349953d80b55992edeb6b8d316c7619f2fa2cd
Brian Alves Moreira <hey@brianmoreira.com>
2022-01-02 21:45:00 -0300

    Cria função para trocar imagens com o click em Soluções na página inicial

--- js/scripts.js
+++ js/scripts.js
@@ -10,6 +10,22 @@ $(document).ready(function(){
 		$('.sec--solutions .card').click( function() {
 			$(this).addClass('active');
 			$(this).siblings().removeClass('active');
+
+			if ($(this).hasClass('one active')) {
+				$('.sec--solutions').addClass('one');
+			} else {
+				$('.sec--solutions').removeClass('one');
+			}
+			if ($(this).hasClass('two active')) {
+				$('.sec--solutions').addClass('two');
+			} else {
+				$('.sec--solutions').removeClass('two');
+			}
+			if ($(this).hasClass('three active')) {
+				$('.sec--solutions').addClass('three');
+			} else {
+				$('.sec--solutions').removeClass('three');
+			}
 		});
 
 		$('.owl-carousel').owlCarousel({
