281cd4bc8d24c4a2d7a2ec214a73ada39d4eba54
Brian Alves Moreira <hey@brianmoreira.com>
2022-01-04 15:35:11 -0300

    Cria versão fixa do header

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,15 @@ $(document).foundation();
 
 var Sheepbar = new Sheepbar();
 
+$(window).on("scroll", function() {
+  if($(window).scrollTop() > 50) {
+    $(".header").addClass("scroll");
+  } else {
+    //remove the background property so it comes transparent again (defined in your css)
+  	$(".header").removeClass("scroll");
+  }
+});
+
 $(document).ready(function(){
 	(function ($) {
 
